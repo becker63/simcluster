88838a1644269466f6c503386bc4215453b11b39
becker <becker@nixos.local>
2025-03-08 16:36:05 -0500

    restructure

--- configurations/node/default.nix
+++ configurations/node/default.nix
@@ -1,3 +1,17 @@
-{
-./configuration.nix
+{ config, lib, pkgs, ... }: {
+  boot.kernelPackages = pkgs.linuxPackages_5_15;
+
+  services.openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "yes";   # Allow root login with a password
+        PasswordAuthentication = true;  # Allow password authentication
+      };
+    };
+
+    users.users.root = {
+      password = "jidw";  # Set an empty password for root
+    };
+
+  system.stateVersion = "23.05";
 }

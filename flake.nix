78e06fe1c275a0aedf47af4fd3142b490f1fed7e
becker <becker@nixos.local>
2025-03-11 22:19:37 -0400

    multi arch too hard

--- flake.nix
+++ flake.nix
@@ -1,22 +1,34 @@
 {
-  description = "A basic flake";
+  description = "packages and build scripts";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.11";
     disko.url = "github:nix-community/disko";
-    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { systems, nixpkgs, disko, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      disko,
+      ...
+    }:
+
     let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
+      system = "x86_64-linux";
       pkgs = import nixpkgs {
+        inherit system;
         config.allowUnfree = true;
       };
+
+      buildImageApp = import ./scripts/build-image.nix { inherit pkgs; };
+      buildISOApp = import ./scripts/build-iso.nix { inherit pkgs; };
     in
     {
+
       nixosConfigurations = {
         build-qcow2 = nixpkgs.lib.nixosSystem {
+          inherit system;
           modules = [
             ./configurations/hardware/sim
             disko.nixosModules.disko
@@ -26,6 +38,8 @@
             (
               { config, ... }:
               {
+                # Adjust this to your liking.
+                # WARNING: if you set a too low value the image might be not big enough to contain the nixos installation
                 disko.devices.disk.main.imageSize = "10G";
               }
             )
@@ -33,8 +47,30 @@
         };
 
         build-iso = nixpkgs.lib.nixosSystem {
+          inherit system;
           modules = [ ./configurations/iso ];
         };
+      };
+
+      # Packages
+      packages.${system} = {
+        buildImage = buildImageApp;
+        buildISO = buildISOApp;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          terraform
+          terraform-providers.libvirt
+          libxslt
+          qemu
+          tree
+        ];
+
+        shellHook = ''
+          # Starting zsh on top of bash for a consistent shell experience
+          zsh
+        '';
       };
     };
 }
